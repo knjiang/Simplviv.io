bf709bff55b6142dc6c4e0896999e472b66f40c3
kjiang2000 <kenjiang@buffalo.edu>
2019-03-04 00:58:00 -0500

    added three endpoints(2 WIP) & data.txt as our persistent db.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
+import unittest
+import requests
+
 from flask import Flask, jsonify
 from flask_cors import CORS
-
 
 # configuration
 DEBUG = True
@@ -8,16 +11,22 @@ DEBUG = True
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
-
 # enable CORS
 CORS(app)
-
-
 # sanity check route
 @app.route('/index', methods=['GET'])
 def main():
     return jsonify('Hello World')
 # chauhiulaam comments
 # 2019 new year
-if __name__ == '__main__':
-    app.run()
+"""
+@app.route('/join', methods=['POST'])
+@app.route('/leave', methods=['POST'])
+"""
+@app.route('/players', methods= ['GET'])
+def players():
+    with open('data.txt') as f:
+        lines = f.read().splitlines()
+        return str(len(lines))
+if __name__ == "__main__":
+    app.run(debug=True)
