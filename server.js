3bd336bc3043ee39cdb8ba4318be951bf93199d9
kjiang2000 <kenjiang@buffalo.edu>
2019-03-25 15:27:31 -0400

    static files are being served on node. insert {node server.js} in bash to run the server.

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
 
-const PORT=8080;
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;
-
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(PORT);
+app.use(express.static(__dirname + '/public'));
+app.get('/main.js', function(req, res){
+    res.sendFile(__dirname + '/main.js');
+});
+app.get('/assets/tilesheet_complete_2X.png', function(req, res){
+    res.sendFile(__dirname + '/assets/tilesheet_complete_2X.png');
+});
+app.get('/assets/map1.json', function(req, res){
+    res.sendFile(__dirname + '/assets/map1.json');
+});
+app.get('/assets/hitman1_gun.png', function(req, res){
+    res.sendFile(__dirname + '/assets/hitman1_gun.png');
+});
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+server.listen(8081, function () {
+    console.log(`Listening on ${server.address().port}`);
 });
