f4bfa4cb0cd3802b9415cbc7fdf7b68551ea2b9f
kjiang2000 <kenjiang@buffalo.edu>
2019-03-25 14:49:32 -0400

    added a sprite and camera movement. TO-DO: camera follows sprite while sprite moves.

--- main.js
+++ main.js
@@ -4,21 +4,45 @@ var config = {
     height: 600,
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update,
+        render: render
     }
 };
 
 var game = new Phaser.Game(config);
+var camera = scene.cameras.main;
 
 function preload(){
     this.load.image('gameTiles', 'assets/tilesheet_complete_2X.png');
-    this.load.tilemapTiledJSON('map', 'assets/demo.json');
+    this.load.tilemapTiledJSON('map', 'assets/map1.json');
+    this.load.image('player','assets/hitman1_gun.png')
 }
 function create(){
     const map = this.make.tilemap({ key : 'map'});
 
     const tileset = map.addTilesetImage('tilesheet_complete_2X','gameTiles');
 
-    const layer1 = map.createStaticLayer("\u5757\u5c42 1", tileset, 0,0);
-    const layer2 = map.createStaticLayer("\u56fe\u5757\u5c42 2", tileset, 0,0);
+    const layer1 = map.createStaticLayer("f1", tileset, 0,0);
+    const camera = this.cameras.main;
+    const cursors = this.input.keyboard.createCursorKeys();
+    controls = new Phaser.Cameras.Controls.FixedKeyControl({
+        camera: camera,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        speed: 0.5
+    });
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    var player = this.add.sprite(200,300,'player')
+    //camera.startFollow(player)
+}
+function update(time,delta) {
+    controls.update(delta);
+}
+function render() {
+
+    game.debug.cameraInfo(game.camera, 32, 32);
+
 }
