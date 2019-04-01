3a16617a1d27250f9868d482880c900bf45145b3
kjiang2000 <kenjiang@buffalo.edu>
2019-03-31 21:28:50 -0400

    added a shooting mechanic(Still WIP)

--- main.js
+++ main.js
@@ -18,18 +18,20 @@ var config = {
 var game = new Phaser.Game(config);
 let cursors;
 let player;
-var spaceKey;
+let spaceKey;
+let TKey;
+let tutorial;
 
 function preload(){
     this.load.image('gameTiles', 'assets/tilesheet_complete_2X.png');
     this.load.tilemapTiledJSON('map', 'assets/map1.json');
-    this.load.image('player','assets/hitman1_gun.png')
+    this.load.image('player','assets/hitman1_gun.png');
+    this.load.image('bullet','assets/bullet.png');
 }
 function create() {
     const map = this.make.tilemap({key: 'map'});
-
     const tileset = map.addTilesetImage('tilesheet_complete_2X', 'gameTiles');
-    const tutorial = this.add.text(0, 0, 'Arrow keys to move', {
+    tutorial = this.add.text(0, 0, 'Arrow keys to move\nSpacebar to shoot', {
             font: "14px Serif",
             padding: {x: 10, y: 10},
             backgroundColor: "##00FFFFFF",
@@ -43,6 +45,45 @@ function create() {
     camera.startFollow(player);
     camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     cursors = this.input.keyboard.createCursorKeys();
+    spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    var Bullet = new Phaser.Class({
+
+        Extends: Phaser.GameObjects.Image,
+
+        initialize:
+
+            function Bullet (scene)
+            {
+                Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+
+                this.speed = Phaser.Math.GetSpeed(400, 1);
+                this.velocity = new Phaser.Geom.Point(0, 0);
+            },
+
+        fire: function (x, y, direction)
+        {
+            this.setPosition(x, y);
+
+            this.setActive(true);
+            this.setVisible(true);
+            this.velocity.setTo(0, -this.speed);
+            Phaser.Math.Rotate(this.velocity, direction);
+        },
+
+        update: function (time, delta) {
+            this.x += this.velocity.x * delta;
+            this.y += this.velocity.y * delta;
+            if (this.y < -50) {
+                this.setActive(false);
+                this.setVisible(false);
+            }
+        }
+    });
+    bullets = this.add.group({
+        classType: Bullet,
+        runChildUpdate: true
+    });
+    bullets.angle = 0
 }
 function update(time,delta) {
     player.body.setVelocity(0);
@@ -65,5 +106,13 @@ function update(time,delta) {
         player.angle = 270;
     } else if (cursors.down.isDown) {
         player.angle = 90;
+    }
+    if (spaceKey.isDown){
+        var bullet = bullets.get();
+
+        if (bullet)
+        {
+            bullet.fire(player.x, player.y,player.angle - 180);
+        }
     }
 }
