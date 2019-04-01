3d712d5c8e73df87ddefde348fdec769c139b101
kjiang2000 <kenjiang@buffalo.edu>
2019-03-31 20:41:06 -0400

    sprite followed by camera, changes direction based on keypress

--- main.js
+++ main.js
@@ -2,47 +2,68 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade:{
+            gravity : {y :0 }
+        }
+    },
     scene: {
         preload: preload,
         create: create,
         update: update,
-        render: render
     }
 };
 
 var game = new Phaser.Game(config);
-var camera = scene.cameras.main;
+let cursors;
+let player;
+var spaceKey;
 
 function preload(){
     this.load.image('gameTiles', 'assets/tilesheet_complete_2X.png');
     this.load.tilemapTiledJSON('map', 'assets/map1.json');
     this.load.image('player','assets/hitman1_gun.png')
 }
-function create(){
-    const map = this.make.tilemap({ key : 'map'});
+function create() {
+    const map = this.make.tilemap({key: 'map'});
 
-    const tileset = map.addTilesetImage('tilesheet_complete_2X','gameTiles');
-
+    const tileset = map.addTilesetImage('tilesheet_complete_2X', 'gameTiles');
+    const tutorial = this.add.text(0, 0, 'Arrow keys to move', {
+            font: "14px Serif",
+            padding: {x: 10, y: 10},
+            backgroundColor: "##00FFFFFF",
+        })
+        .setScrollFactor(0)
+        .setDepth(30);
     const layer1 = map.createStaticLayer("f1", tileset, 0,0);
     const camera = this.cameras.main;
-    const cursors = this.input.keyboard.createCursorKeys();
-    controls = new Phaser.Cameras.Controls.FixedKeyControl({
-        camera: camera,
-        left: cursors.left,
-        right: cursors.right,
-        up: cursors.up,
-        down: cursors.down,
-        speed: 0.5
-    });
+    player = this.physics.add.sprite(200,300,'player')
+    player.angle = 0;
+    camera.startFollow(player);
     camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-    var player = this.add.sprite(200,300,'player')
-    //camera.startFollow(player)
+    cursors = this.input.keyboard.createCursorKeys();
 }
 function update(time,delta) {
-    controls.update(delta);
-}
-function render() {
-
-    game.debug.cameraInfo(game.camera, 32, 32);
-
+    player.body.setVelocity(0);
+    if (cursors.left.isDown) {
+        player.body.setVelocityX(-200);
+    } else if (cursors.right.isDown) {
+        player.body.setVelocityX(200);
+    }
+    if (cursors.up.isDown) {
+        player.body.setVelocityY(-200);
+    } else if (cursors.down.isDown) {
+        player.body.setVelocityY(200);
+    }
+    player.body.velocity.normalize().scale(200);
+    if (cursors.left.isDown) {
+        player.angle = 180;
+    } else if (cursors.right.isDown) {
+        player.angle = 0;
+    } else if (cursors.up.isDown) {
+        player.angle = 270;
+    } else if (cursors.down.isDown) {
+        player.angle = 90;
+    }
 }
